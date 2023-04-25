5fff0a510ab058574c37c5986c76cbdc4652d703
willsun <willsun@lilith.com>
2023-04-25 16:19:30 +0800

    优化ginstarter

--- ginstarter/Utils.go
+++ ginstarter/Utils.go
@@ -2,10 +2,11 @@ package ginstarter
 
 import (
 	"github.com/gin-gonic/gin"
+	ginlogger "github.com/why2go/gostarter/ginstarter/logger"
 )
 
 func GetRequestIdFromGinContext(ctx *gin.Context) string {
-	v := ctx.Value("request-id")
+	v := ctx.Value(ginlogger.RequestIdKey)
 	if reqId, ok := v.(string); ok {
 		return reqId
 	} else {
